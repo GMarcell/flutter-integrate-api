5d2bfdaecf266c23a5bff14eae63db7bb5753f6e
Grand Marcell <grand1310marcell@gmail.com>
2023-07-04 16:59:17 +0700

    add object model

--- lib/screen/home.dart
+++ lib/screen/home.dart
@@ -2,6 +2,7 @@ import 'dart:convert';
 
 import 'package:flutter/material.dart';
 import 'package:http/http.dart' as http;
+import 'package:integrate_api/model/user.dart';
 
 class HomeScreen extends StatefulWidget {
   const HomeScreen({super.key});
@@ -11,7 +12,7 @@ class HomeScreen extends StatefulWidget {
 }
 
 class _HomeScreenState extends State<HomeScreen> {
-  List<dynamic> users = [];
+  List<User> users = [];
 
   @override
   Widget build(BuildContext context) {
@@ -23,17 +24,10 @@ class _HomeScreenState extends State<HomeScreen> {
           itemCount: users.length,
           itemBuilder: (context, index) {
             final user = users[index];
-            final fname = user['name']['first'];
-            final lname = user['name']['last'];
-            final email = user['email'];
-            final imageUrl = user['picture']['thumbnail'];
+            // final color = user.gender == 'male' ? Colors.blue : Colors.green;
             return ListTile(
-              leading: ClipRRect(
-                borderRadius: BorderRadius.circular(100),
-                child: Image.network(imageUrl)
-              ),
-              title: Text('$fname $lname'),
-              subtitle: Text(email),
+              title: Text('${user.name.title} ${user.name.first} ${user.name.last}'),
+              subtitle: Text(user.phone),
             );
           }),
       floatingActionButton: FloatingActionButton(
@@ -44,13 +38,29 @@ class _HomeScreenState extends State<HomeScreen> {
 
   void fetchUsers() async {
     print('called');
-    const url = 'https://randomuser.me/api/?results=10';
+    const url = 'https://randomuser.me/api/?results=100';
     final uri = Uri.parse(url);
     final response = await http.get(uri);
     final body = response.body;
     final json = jsonDecode(body);
+    final result = json['results'] as List<dynamic>;
+    final transform = result.map((e) {
+      final name = UserName(
+        title: e['name']['title'],
+        first: e['name']['first'],
+        last: e['name']['last'],
+      );
+      return User(
+          gender: e['gender'],
+          email: e['email'],
+          phone: e['phone'],
+          cell: e['cell'],
+          nat: e['nat'],
+          name: name
+        );
+    }).toList();
     setState(() {
-      users = json['results'];
+      users = transform;
     });
   }
 }
